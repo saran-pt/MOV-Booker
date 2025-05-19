256b7a81b1f7e29d8680df1a85c55689d0b5226e
saran-pt <sarantocs@gmail.com>
2025-05-19 17:56:09 +0530

    added crud functions

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	// "strconv"
 	"encoding/json"
 	// "reflect"
+	"math/rand"
+	"strconv"
 	"github.com/gorilla/mux"
 )
 
@@ -20,7 +22,6 @@ type Director struct {
 	FirstName string `json:"firstname"`
 	LastName string `json:"lastname"`
 }
-
 
 func home(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to Movie Buzz...");
@@ -48,8 +49,44 @@ func getMovie(w http.ResponseWriter, r *http.Request) {
 
 
 func createMovie(w http.ResponseWriter, r *http.Request) {
-	params := http.NewDecoder(r).Decode(&Movie)
-	fmt.Println(params)
+	w.Header().Set("Content-Type", "application/json")
+	var movie Movie
+	_ = json.NewDecoder(r.Body).Decode(&movie)
+	movie.Id = strconv.Itoa(rand.Intn(1000000))
+ 	movies = append(movies, movie)
+}
+
+
+func updateMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+
+	for index, obj := range movies {
+		if obj.Id == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+			break
+		}
+	}
+
+	var movie Movie
+	_ = json.NewDecoder(r.Body).Decode(&movie)
+	movie.Id = params["id"]
+	movies = append(movies, movie)
+	json.NewEncoder(w).Encode(&movie)
+}
+
+
+func deleteMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	params := mux.Vars(r)
+	for index, obj := range movies {
+		if obj.Id == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+			_ = json.NewEncoder(w).Encode(&obj)
+			return
+		}
+	}
 }
 
 
@@ -57,17 +94,18 @@ var movies []Movie
 
 func main() {
 	// Movie Data
-	movies = append(movies, Movie{"101", "OkOK", &Director{"Ram", "Gopal"}})
-	movies = append(movies, Movie{"102", "Mind.Off", &Director{"Pream", "Hank"}})
+	// movies = append(movies, Movie{"101", "OkOK", &Director{"Ram", "Gopal"}})
+	// movies = append(movies, Movie{"102", "Mind.Off", &Director{"Pream", "Hank"}})
 	fmt.Println("Server is running...");
 
 	r := mux.NewRouter()
 	
 	r.HandleFunc("/", home).Methods("GET")
 	r.HandleFunc("/movies", getMovies).Methods("GET")
-	r.HandleFunc("/movie/{id}", getMovie).Methods("GET")
+	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
 	r.HandleFunc("/movies", createMovie).Methods("POST")
-	// r.HandleFunc("/movies", createMovie).Methods("POST")
+	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
 
 	log.Fatal(http.ListenAndServe(":8080", r));
 }
